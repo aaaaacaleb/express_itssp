ecd1b7237e8f97b7a1bf5255db0fe33ee51fc0de
Caleb <hal9000.2@outlook.com>
2025-10-22 11:50:16 -0600

    4to_agregamos datos y ciclo forEach para productos

--- index.js
+++ index.js
@@ -6,10 +6,45 @@ app.use(express.json());
 app.set('view engine','ejs');
 
 app.get('/inicio', (req, res) => {
-    res.render('inicio',{
-        titulo: 'Pagina de inicio',
-        mensaje: 'Bienvenido a nuestra tienda en linea'
-    });
+    const productos = [
+       { 
+        nombre: 'Audifonos bluethooth',
+        descripción: 'Sonido de alta calidad y cancelación de ruido',
+        precio: 899.99,
+        imagen: 'https://picsum.photos/200'
+       }, 
+       { 
+        nombre: 'SmartWatch Pro',
+        descripción: 'Controla tu salud y recibe notificaciones',
+        precio: 1299.50,
+        imagen: 'https://picsum.photos/200?2'
+       },
+       { 
+        nombre: 'Camara 4K',
+        descripción: 'Captura tus momentos con resolución Ultra HD',
+        precio: 4999.00,
+        imagen: 'https://picsum.photos/200?3'
+       },
+       { 
+        nombre: 'Television Samsung',
+        descripción: 'Pantalla de alta calidad y colores',
+        precio: 900.50,
+        imagen: 'https://picsum.photos/200?4'
+       }, 
+       { 
+        nombre: 'Smartphone',
+        descripción: 'Telefono Inteligente con gran almacenamiento',
+        precio: 1000.00,
+        imagen: 'https://picsum.photos/200?5'
+       },
+       { 
+        nombre: 'Computadora Personal',
+        descripción: 'Computadora con gran procesamiento',
+        precio: 850.50,
+        imagen: 'https://picsum.photos/200?6'
+       }
+    ];
+    res.render('inicio',{ productos: productos });
 });
 
 
