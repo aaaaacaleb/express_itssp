c7fe4476cb78885d4fb6f2ec274d7843aee8bcdc
Caleb <hal9000.2@outlook.com>
2025-10-23 09:45:29 -0600

    Mandamos llamar los datos de nuestra tabla de la base de datos

--- index.js
+++ index.js
@@ -5,46 +5,35 @@ app.use(express.json());
 //motor de views
 app.set('view engine','ejs');
 
-app.get('/inicio', (req, res) => {
-    const productos = [
-       { 
-        nombre: 'Audifonos bluethooth',
-        descripción: 'Sonido de alta calidad y cancelación de ruido',
-        precio: 899.99,
-        imagen: 'https://picsum.photos/200'
-       }, 
-       { 
-        nombre: 'SmartWatch Pro',
-        descripción: 'Controla tu salud y recibe notificaciones',
-        precio: 1299.50,
-        imagen: 'https://picsum.photos/200?2'
-       },
-       { 
-        nombre: 'Camara 4K',
-        descripción: 'Captura tus momentos con resolución Ultra HD',
-        precio: 4999.00,
-        imagen: 'https://picsum.photos/200?3'
-       },
-       { 
-        nombre: 'Television Samsung',
-        descripción: 'Pantalla de alta calidad y colores',
-        precio: 900.50,
-        imagen: 'https://picsum.photos/200?4'
-       }, 
-       { 
-        nombre: 'Smartphone',
-        descripción: 'Telefono Inteligente con gran almacenamiento',
-        precio: 1000.00,
-        imagen: 'https://picsum.photos/200?5'
-       },
-       { 
-        nombre: 'Computadora Personal',
-        descripción: 'Computadora con gran procesamiento',
-        precio: 850.50,
-        imagen: 'https://picsum.photos/200?6'
-       }
-    ];
-    res.render('inicio',{ productos: productos });
+//express
+const admin = require('firebase-admin');
+const cors = require('cors');
+app.use(cors());
+app.use(express.urlencoded({ extended:true }));
+
+//credenciales
+const serviceAccount = require('./firebase_key.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();// Rutas de la API de productos
+
+app.get("/productos", async (req, res) => {
+    try{
+        const items = await db.collection("productos").get();
+        const productos = items.docs.map(doc => {
+            const data = doc.data();
+            return {
+                id: doc.id,
+                nombre: data.nombre,
+                precio: data.precio,
+                imagen: data.imagen
+            };
+        });
+        res.render('inicio', {productos});
+    } catch (error) {
+      res.status(500).json({ error: error.message});
+    }
 });
 
 
